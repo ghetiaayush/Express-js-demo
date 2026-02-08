ba2e65124fca0cef559c7ae2a08063d6a12e2fca
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2026-02-08 08:11:44 +0530

    Express JS & Setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("", (req, resp) => {
+  resp.send("<h1>Basic Node js example</h1>");
+});
+
+// app.get("/about", (req, resp) => {
+//   resp.send("<h1>This is about page</h1>");
+// });
+
+// app.get("/contact", (req, resp) => {
+//   resp.send("<h1>This is contact page</h1>");
+// });
+
+app.listen(3200);
