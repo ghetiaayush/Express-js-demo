2f3f27de0f9c35bba5929575d1aa84aa07a5ddf5
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2026-05-17 15:00:20 +0530

    404 page and changes in website structure

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import home from "./pages/home.js";
-import about from "./pages/about.js";
-import contact from "./pages/contact.js";
-import demo, { demo2 } from "./pages/demo.js"; //example of more than one export of function from same file
-import login from "./pages/login.js";
+import path from "path";
+
+// import home from "./pages/home.js";
+// import about from "./pages/about.js";
+// import contact from "./pages/contact.js";
+// import demo, { demo2 } from "./pages/demo.js"; //example of more than one export of function from same file
+// import login from "./pages/login.js";
 import submit from "./pages/submit.js";
 
 
@@ -13,33 +15,51 @@ const app = express();
 app.use(express.static('public'));
 
 app.get("/", (req, resp) => {
-  resp.send(home());
+  // resp.send(home());
+  const absPath = path.resolve("view/home.html");
+  resp.sendFile(absPath);
 });
 
-app.get("/about", (req, resp) => {
-  resp.send(about());
-});
 
-app.get("/contact", (req, resp) => {
-  resp.send(contact());
-});
-
-//  Render html elements and forms
 app.get("/login", (req, resp) => {
-  resp.send(login());
+  // resp.send(login());
+  const absPath = path.resolve("view/login.html");
+  resp.sendFile(absPath);
 });
 
-/* When the form is submitted, this route handles the POST request and returns a success message. */
+ /* When the form is submitted, this route handles the POST request and returns a success message. */
 app.post("/submit", (req, resp) => {
   resp.send(submit());
 });
 
-app.get("/demo", (req, resp) => {
-   resp.send(demo());
+app.get("/about", (req, resp) => {
+  //resp.send(about());
+  const absPath = path.resolve("view/about.html");
+  resp.sendFile(absPath);
 });
 
-app.get("/demo2", (req, resp) => {
-   resp.send(demo2())});
+app.get("/contact", (req, resp) => {
+  //resp.send(about());
+  const absPath = path.resolve("view/contact.html");
+  resp.sendFile(absPath);
+});
+
+//CODE FOR 404 error page
+app.use((req, resp) => {
+  const absPath = path.resolve("view/404.html");
+  resp.status(404).sendFile(absPath);
+});
+
+
+// app.get("/contact", (req, resp) => {
+//   resp.send(contact());
+// });
+// app.get("/demo", (req, resp) => {
+//    resp.send(demo());
+// });
+
+// app.get("/demo2", (req, resp) => {
+//    resp.send(demo2())});
 
 app.listen(3200);
 
@@ -54,6 +74,7 @@ app.post() – Handles POST requests.
 req – Represents the request object.
 resp – Represents the response object.
 resp.send() – Sends HTML or plain text response.
+resp.sendFile() – Sends a file as response.
 app.listen(3200) – Server listens on port 3200.
 
 */
